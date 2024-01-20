e12c71b5c9c115126c3bfedc8158245201bdf8f5
elias11mendez <mendezeliasjosue58@gmail.com>
2024-01-20 10:44:55 -0600

    Primera version del Website

---
+++ script/scroller.js
@@ -0,0 +1,6 @@
+function scrollToContact() {
+    document.querySelector('#contacto').scrollIntoView({ behavior: 'smooth' });
+}
+function scrollToHome(){
+    document.querySelector('#home').scrollIntoView({behavior: 'smooth'})
+}
